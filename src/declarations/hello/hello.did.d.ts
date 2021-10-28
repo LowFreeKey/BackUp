60579f53c834625f51e142cb8eb145e70ccf056b
LowFreeKey <33438220+LowFreeKey@users.noreply.github.com>
2021-10-28 23:06:28 +0100

    Declarations in TypeScript

--- src/declarations/hello/hello.did.d.ts
+++ src/declarations/hello/hello.did.d.ts
@@ -1,5 +1,27 @@
 import type { Principal } from '@dfinity/principal';
+export interface EssayEntry {
+  'title' : string,
+  'topic' : string,
+  'wordCount' : number,
+  'userId' : Principal,
+  'text' : string,
+  'tokenToPay' : number,
+  'reviewTimes' : number,
+}
+export interface UserEntry {
+  'userName' : string,
+  'token' : number,
+  'myEssays' : Array<bigint>,
+  'userRating' : number,
+  'reviewingEssay' : Array<bigint>,
+}
 export interface _SERVICE {
-  'greet' : (arg_0: string) => Promise<string>,
-  'test' : () => Promise<string>,
+  'addReviewingEssay' : (arg_0: bigint) => Promise<undefined>,
+  'createEssay' : (arg_0: EssayEntry) => Promise<undefined>,
+  'createProfile' : (arg_0: UserEntry) => Promise<boolean>,
+  'getAllEssays' : () => Promise<Array<[bigint, EssayEntry]>>,
+  'getEssay' : (arg_0: bigint) => Promise<[] | [EssayEntry]>,
+  'getUserEntrybyPrincipal' : () => Promise<[] | [UserEntry]>,
+  'logIn' : () => Promise<boolean>,
+  'whoami' : () => Promise<Principal>,
 }
--- src/declarations/hello/hello.did.js
+++ src/declarations/hello/hello.did.js
@@ -1,7 +1,33 @@
 export const idlFactory = ({ IDL }) => {
+  const EssayEntry = IDL.Record({
+    'title' : IDL.Text,
+    'topic' : IDL.Text,
+    'wordCount' : IDL.Nat32,
+    'userId' : IDL.Principal,
+    'text' : IDL.Text,
+    'tokenToPay' : IDL.Nat32,
+    'reviewTimes' : IDL.Nat32,
+  });
+  const UserEntry = IDL.Record({
+    'userName' : IDL.Text,
+    'token' : IDL.Nat32,
+    'myEssays' : IDL.Vec(IDL.Nat),
+    'userRating' : IDL.Nat32,
+    'reviewingEssay' : IDL.Vec(IDL.Nat),
+  });
   return IDL.Service({
-    'greet' : IDL.Func([IDL.Text], [IDL.Text], ['query']),
-    'test' : IDL.Func([], [IDL.Text], ['query']),
+    'addReviewingEssay' : IDL.Func([IDL.Nat], [], ['oneway']),
+    'createEssay' : IDL.Func([EssayEntry], [], []),
+    'createProfile' : IDL.Func([UserEntry], [IDL.Bool], []),
+    'getAllEssays' : IDL.Func(
+        [],
+        [IDL.Vec(IDL.Tuple(IDL.Nat, EssayEntry))],
+        [],
+      ),
+    'getEssay' : IDL.Func([IDL.Nat], [IDL.Opt(EssayEntry)], []),
+    'getUserEntrybyPrincipal' : IDL.Func([], [IDL.Opt(UserEntry)], []),
+    'logIn' : IDL.Func([], [IDL.Bool], []),
+    'whoami' : IDL.Func([], [IDL.Principal], []),
   });
 };
 export const init = ({ IDL }) => { return []; };
