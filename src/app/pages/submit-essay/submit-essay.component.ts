670669ad33a76150f74979d8d25aac7625862114
marvincd <61430224+marvincd@users.noreply.github.com>
2021-11-06 10:02:24 +0000

    validation mvp

--- src/app/pages/submit-essay/submit-essay.component.ts
+++ src/app/pages/submit-essay/submit-essay.component.ts
@@ -20,25 +20,34 @@ export class SubmitEssayComponent implements OnInit {
     this.EssayObj = {
       title : '',
       topic : '',
-      wordCount : 9,
+      wordCount : 0,
       text : '',
-      tokenToPay : 3,
-      reviewTimes : 4,
+      tokenToPay : 0,
+      reviewTimes : 0,
     };
   }
   changeRoute(): void{
     this.router.navigate(['../my-profile'], { relativeTo: this.route });
-  }
+  }  
   async changeRouteSkyboard(){
-    console.log(this.EssayObj);
-    this.userId = this.helloService.userId;
-    this.response =  await this.helloService.createEssay(this.EssayObj);
-    console.log(this.response);
-    this.essays = await this.helloService.getAllEssay();
-    console.log(this.essays);
-    this.essay = await this.helloService.getEssay(4);
-    console.log(this.essay);
     this.router.navigate(['../skyboard'], { relativeTo: this.route });
+  }
+  async Submit(essayForm: { valid: any; }){
+    if (essayForm.valid) {      
+      console.log(this.EssayObj);
+      this.userId = this.helloService.userId;
+      this.response =  await this.helloService.createEssay(this.EssayObj);
+      console.log(this.response);
+      this.essays = await this.helloService.getAllEssay();
+      console.log(this.essays);
+      this.essay = await this.helloService.getEssay(4);
+      console.log(this.essay);
+      this.router.navigate(['../skyboard'], { relativeTo: this.route });
+       } 
+    else {
+            console.error('Stock form is in an invalid state');   
+     } 
+   
   }
   changeRouteEssayReview(){
     this.router.navigate(['../essay-review'], { relativeTo: this.route });
