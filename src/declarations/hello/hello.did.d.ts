97cdf8162ff0df57919eae4fbca2b845610e277c
LowFreeKey <33438220+LowFreeKey@users.noreply.github.com>
2022-01-12 18:51:53 +0100

    Final fixes to live deployment
    Fixing Angular Budget issue
    + 403 Content type forbidden for controller id.

---
+++ src/declarations/hello/hello.did.d.ts
@@ -0,0 +1,36 @@
+import type { Principal } from '@dfinity/principal';
+export interface EssayEntry {
+  'title' : string,
+  'topic' : string,
+  'wordCount' : number,
+  'text' : string,
+  'tokenToPay' : bigint,
+  'reviewTimes' : number,
+}
+export interface UserEntry {
+  'userName' : string,
+  'token' : bigint,
+  'myEssays' : Array<bigint>,
+  'pastRatedFeedbacks' : Array<bigint>,
+  'userRating' : bigint,
+  'reviewingEssay' : bigint,
+}
+export interface _SERVICE {
+  'addRating' : (arg_0: bigint, arg_1: bigint) => Promise<undefined>,
+  'addReviewingEssay' : (arg_0: bigint) => Promise<boolean>,
+  'createEssay' : (arg_0: EssayEntry) => Promise<boolean>,
+  'createProfile' : (arg_0: UserEntry) => Promise<boolean>,
+  'deleteAllEssays' : () => Promise<undefined>,
+  'deleteEssay' : (arg_0: bigint) => Promise<undefined>,
+  'getAllEssays' : () => Promise<Array<[bigint, EssayEntry]>>,
+  'getEssay' : (arg_0: bigint) => Promise<[] | [EssayEntry]>,
+  'getEssaysFromUser' : () => Promise<Array<EssayEntry>>,
+  'getReviewingEssay' : () => Promise<[] | [EssayEntry]>,
+  'getReviewsFromEssay' : (arg_0: bigint) => Promise<string>,
+  'getUserEntrybyPrincipal' : () => Promise<[] | [UserEntry]>,
+  'incomeByAvarage' : (arg_0: bigint) => Promise<bigint>,
+  'logIn' : () => Promise<boolean>,
+  'pay' : (arg_0: UserEntry, arg_1: bigint) => Promise<UserEntry>,
+  'submittReviewedEssay' : (arg_0: string) => Promise<undefined>,
+  'whoami' : () => Promise<Principal>,
+}
