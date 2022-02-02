3c9331e8529d44c76819c70ffa769c07c7409c47
marvincd <61430224+marvincd@users.noreply.github.com>
2022-02-02 19:42:58 +0000

    quilljs commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { R3TargetBinder } from '@angular/compiler';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'google-docs';
+export class AppComponent implements OnInit{
+  public editorForm!: FormGroup;
+  public editorStyle ={
+    height: '300px;',
+    backgroundColor: '#000;'
+  }
+
+ public response!:any;
+  ngOnInit(): void {
+  this.editorForm = new FormGroup({
+    'editor': new FormControl(null)
+  })
+    
+  }
+   onSubmit(){
+    console.log(this.editorForm.get('editor'))
+  } 
 }
+
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+
+import { QuillModule } from 'ngx-quill';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,6 +13,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    QuillModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [],
